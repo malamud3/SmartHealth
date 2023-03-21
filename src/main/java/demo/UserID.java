3f8f6d1454e2e5d7109d3c4cac1e14ccc5e4251c
gilaz <gilaz@gilazani>
2023-03-21 20:52:16 +0200

    url fix

--- src/main/java/demo/UserID.java
+++ src/main/java/demo/UserID.java
@@ -14,7 +14,12 @@ public class UserID {
 		this.email = email;
 		this.superApp = "2023b.Gil.Azani"; //TODO change to constant!
 	}
-
+	
+	public UserID(String superapp, String email) {
+		super();
+		this.email = email;
+		this.superApp = superapp;
+	}
 
 	public String getSuperApp() {
 		return superApp;
