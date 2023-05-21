52af74c9074853b37058f2481e55a7779df12702
amirmalamud <malamud.amir@s.afeka.ac.il>
2023-05-21 13:01:02 +0300

    fixing

--- src/main/java/superapp/logic/service/UsersServiceWithAdminPermission.java
+++ src/main/java/superapp/logic/service/UsersServiceWithAdminPermission.java
@@ -3,7 +3,13 @@ package superapp.logic.service;
 import superapp.Boundary.User.UserId;
 
 public interface UsersServiceWithAdminPermission extends UsersService {
-	
+
 	public void deleteAllUsers(UserId userId) throws RuntimeException;
+
+	public void exportAllUsers(String superapp,String userEmail,String userSuperApp,int size, int page ) throws RuntimeException;
+
+	public void exportAllCommands(String superapp,String userEmail,String userSuperApp,int size, int page ) throws RuntimeException;
+
+	public void exportSpecificCommands(String superapp,String userEmail,String userSuperApp,int size, int page ) throws RuntimeException;
 
 }
