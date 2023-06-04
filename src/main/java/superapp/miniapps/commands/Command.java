4f6a71218b64aabdb7266c9ed4d36d187eedd517
LiwaaHosh <66652532+LiwaaCoder@users.noreply.github.com>
2023-06-05 00:56:46 +0300

    commit

---
+++ src/main/java/superapp/miniapps/commands/Command.java
@@ -0,0 +1,9 @@
+package superapp.miniapps.commands;
+
+import superapp.Boundary.MiniAppCommandBoundary;
+import superapp.Boundary.SuperAppObjectBoundary;
+
+public interface Command {
+    public void execute(MiniAppCommandBoundary miniAppCommandBoundary);
+}
+
