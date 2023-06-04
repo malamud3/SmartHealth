53145a49bbdb0b0892cf4eaf61845c8da6d54f3f
amirmalamud <malamud.amir@s.afeka.ac.il>
2023-06-04 12:00:14 +0300

    mongo

--- src/main/java/superapp/Application.java
+++ src/main/java/superapp/Application.java
@@ -2,15 +2,15 @@ package superapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext context = SpringApplication.run(Application.class, args);
 
     }
 
