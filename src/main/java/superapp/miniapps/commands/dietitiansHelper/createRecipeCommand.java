5520d855d9371f9b27a7e5a616953991b238558a
amirmalamud <malamud.amir@s.afeka.ac.il>
2023-06-04 18:47:46 +0300

    fix

--- src/main/java/superapp/miniapps/commands/dietitiansHelper/createRecipeCommand.java
+++ src/main/java/superapp/miniapps/commands/dietitiansHelper/createRecipeCommand.java
@@ -1,17 +1,22 @@
 package superapp.miniapps.commands.dietitiansHelper;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import superapp.Boundary.MiniAppCommandBoundary;
 import superapp.Boundary.SuperAppObjectBoundary;
 import superapp.dal.SuperAppObjectCrud;
 import superapp.dal.UserCrud;
 import superapp.data.IngredientEntity;
+import superapp.data.UserEntity;
+import superapp.logic.Mongo.ObjectServiceRepo;
 import superapp.logic.service.SpoonaculerService;
 import superapp.logic.service.SuperAppObjService.SuperAppObjectRelationshipService;
+import superapp.logic.utilitys.UserUtility;
 
 @Component("createRecipe")
-public class CreateRecipeCommand {
+public class createRecipeCommand {
+
     private SpoonaculerService spoonaculerService;
     private SuperAppObjectCrud superAppObjectCrud;
     private UserCrud userCrud;
@@ -25,7 +30,7 @@ public class CreateRecipeCommand {
         this.objectRelationshipService = objectRelationshipService;
     }
 
-    public SuperAppObjectBoundary execute(MiniAppCommandBoundary commandBoundary) {
+    public SuperAppObjectBoundary createRecipe(MiniAppCommandBoundary commandBoundary) {
         // 1. find the dietitian object
         // 2. add new recipe to the dietitian object
         String superapp = commandBoundary.getCommandId().getSuperapp();
@@ -42,4 +47,5 @@ public class CreateRecipeCommand {
 
         return dietitian;
     }
+
 }
