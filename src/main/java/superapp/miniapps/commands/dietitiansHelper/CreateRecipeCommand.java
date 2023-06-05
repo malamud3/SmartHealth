15764c085abb746c6478c47ff58a8907640c045b
amirmalamud <malamud.amir@s.afeka.ac.il>
2023-06-05 18:22:22 +0300

    fix

--- src/main/java/superapp/miniapps/commands/dietitiansHelper/CreateRecipeCommand.java
+++ src/main/java/superapp/miniapps/commands/dietitiansHelper/CreateRecipeCommand.java
@@ -4,27 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import superapp.Boundary.MiniAppCommandBoundary;
 import superapp.Boundary.ObjectId;
-import superapp.Boundary.SuperAppObjectBoundary;
 import superapp.dal.SuperAppObjectCrud;
-import superapp.dal.UserCrud;
 import superapp.data.SuperAppObjectEntity;
-import superapp.logic.Mongo.ObjectServiceRepo;
-import superapp.logic.service.SuperAppObjService.ObjectServicePaginationSupported;
-import superapp.logic.service.SuperAppObjService.ObjectsServiceWithAdminPermission;
 import superapp.logic.utilitys.SuperAppObjectUtility;
-import superapp.logic.utilitys.UserUtility;
 import superapp.miniapps.commands.Command;
 
 @Component("CREATE_RECIPE")
 public class CreateRecipeCommand implements Command {
     //private final  SpoonaculerService spoonaculerService;
     private SuperAppObjectUtility superAppObjectUtility;
-    
+
     private SuperAppObjectCrud objectRepository;
 
     @Autowired
     public CreateRecipeCommand(SuperAppObjectCrud objectRepository) {
-
         //  this.spoonaculerService = spoonaculerService;
         this.objectRepository = objectRepository;
         this.superAppObjectUtility = new SuperAppObjectUtility(objectRepository);
@@ -41,7 +34,7 @@ public class CreateRecipeCommand implements Command {
         dietitian.insertToObjectDetails(miniAppCommandBoundary.getCommandAttributes().get("recipeName").toString(),
         		miniAppCommandBoundary.getCommandAttributes().get("recipeDetails"));
         objectRepository.save(dietitian);
-        
+
         //entity to boundary
         return dietitian;
     }
