54f3c2ad5630791a247fce25c2c7325e48df9611
amirmalamud <malamud.amir@s.afeka.ac.il>
2023-06-05 19:56:43 +0300

    fix

--- src/main/java/superapp/miniapps/commands/dietitiansHelper/GetAllRecipeCommand.java
+++ src/main/java/superapp/miniapps/commands/dietitiansHelper/GetAllRecipeCommand.java
@@ -1,4 +1,38 @@
 package superapp.miniapps.commands.dietitiansHelper;
 
-public class GetAllRecipeCommand {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import superapp.Boundary.MiniAppCommandBoundary;
+import superapp.Boundary.ObjectId;
+import superapp.dal.SuperAppObjectCrud;
+import superapp.data.SuperAppObjectEntity;
+import superapp.logic.utilitys.SuperAppObjectUtility;
+import superapp.miniapps.commands.Command;
+
+@Component("GET_ALL_RECIPES")
+public class GetAllRecipeCommand implements Command {
+    //private final  SpoonaculerService spoonaculerService;
+    private SuperAppObjectUtility superAppObjectUtility;
+
+    private SuperAppObjectCrud objectRepository;
+
+    @Autowired
+    public GetAllRecipeCommand(SuperAppObjectCrud objectRepository) {
+        //  this.spoonaculerService = spoonaculerService;
+        this.objectRepository = objectRepository;
+        this.superAppObjectUtility = new SuperAppObjectUtility(objectRepository);
+    }
+
+    @Override
+    public Object execute(MiniAppCommandBoundary miniAppCommandBoundary) {
+        // 1. find the dietitian object
+        // 2. add new recipe to the dietitian object
+        ObjectId idObject = miniAppCommandBoundary.getTargetObject().getObjectId();
+        SuperAppObjectEntity dietitian = superAppObjectUtility.checkSuperAppObjectEntityExist(idObject);
+        dietitian.getObjectDetails().get(miniAppCommandBoundary.getCommandAttributes().get("recipeName").toString());
+        objectRepository.save(dietitian);
+
+        //entity to boundary
+        return dietitian;
+    }
 }
--- src/main/java/superapp/miniapps/commands/dietitiansHelper/GetRecipeCommand.java
+++ src/main/java/superapp/miniapps/commands/dietitiansHelper/GetRecipeCommand.java
@@ -1,4 +1,38 @@
 package superapp.miniapps.commands.dietitiansHelper;
 
-public class GetRecipeCommand {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import superapp.Boundary.MiniAppCommandBoundary;
+import superapp.Boundary.ObjectId;
+import superapp.dal.SuperAppObjectCrud;
+import superapp.data.SuperAppObjectEntity;
+import superapp.logic.utilitys.SuperAppObjectUtility;
+import superapp.miniapps.commands.Command;
+
+@Component("GET_RECIPE")
+public class GetRecipeCommand implements Command {
+    //private final  SpoonaculerService spoonaculerService;
+    private SuperAppObjectUtility superAppObjectUtility;
+
+    private SuperAppObjectCrud objectRepository;
+
+    @Autowired
+    public GetRecipeCommand(SuperAppObjectCrud objectRepository) {
+        //  this.spoonaculerService = spoonaculerService;
+        this.objectRepository = objectRepository;
+        this.superAppObjectUtility = new SuperAppObjectUtility(objectRepository);
+    }
+
+    @Override
+    public Object execute(MiniAppCommandBoundary miniAppCommandBoundary) {
+        // 1. find the dietitian object
+        // 2. add new recipe to the dietitian object
+        ObjectId idObject = miniAppCommandBoundary.getTargetObject().getObjectId();
+        SuperAppObjectEntity dietitian = superAppObjectUtility.checkSuperAppObjectEntityExist(idObject);
+        dietitian.getObjectDetails().get(miniAppCommandBoundary.getCommandAttributes().get("recipeName").toString());
+        objectRepository.save(dietitian);
+
+        //entity to boundary
+        return dietitian;
+    }
 }
