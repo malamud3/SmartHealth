f38064c3faa5bc7276da5d1c5139349e2d40d9b5
amirmalamud <malamud.amir@s.afeka.ac.il>
2023-06-05 18:24:15 +0300

    fix

--- src/main/java/superapp/miniapps/commands/RecipesCommandFactory.java
+++ src/main/java/superapp/miniapps/commands/RecipesCommandFactory.java
@@ -14,21 +14,8 @@ import superapp.miniapps.commands.dietitiansHelper.CreateRecipeCommand;
 
 @Service
 public class RecipesCommandFactory implements RecipesCommandInterface {
-    private SuperAppObjectCrud objectRepository;
-    private UserCrud userCrud;
-    private CreateRecipeCommand createRecipeCommand;
+
     private ApplicationContext applicationContext;
-
-//    public RecipesCommandFactory() {
-    
-//    }
-    @Autowired
-    public RecipesCommandFactory(SuperAppObjectCrud objectRepository, UserCrud userCrud) {
-
-        this.objectRepository = objectRepository;
-        this.userCrud = userCrud;
-        this.createRecipeCommand = new CreateRecipeCommand(objectRepository,userCrud);
-    }
 
     @Autowired
     public void setApplicationContext(ApplicationContext applicationContext) {
@@ -36,36 +23,9 @@ public class RecipesCommandFactory implements RecipesCommandInterface {
     }
     @Override
     public Object createCommand(CommandsEnum commandsEnum, MiniAppCommandBoundary commandBoundary) {
-//        switch (commandsEnum) {
-//            case CREATE_RECIPE -> {
-//                createRecipe(commandBoundary);
-//                System.out.println("Processing createRecipe");
-//            }
-//            case MODIFY_RECIPE -> {
-//                modifyRecipe(commandBoundary);
-//                System.out.println("Processing modifyRecipe");
-//            }
-//            case DELETE_RECIPE -> {
-//                deleteRecipe(commandBoundary);
-//                System.out.println("Processing deleteRecipe");
-//            }
-//            case DELETE_ALL_RECIPES -> {
-//                deleteAllRecipes(commandBoundary);
-//                System.out.println("Processing deleteAllRecipes");
-//            }
-//            case GET_RECIPE -> {
-//                getRecipe(commandBoundary);
-//                System.out.println("Processing getRecipe");
-//            }
-//            case GET_ALL_RECIPES -> {
-//                getAllRecipe(commandBoundary);
-//                System.out.println("Processing getAllRecipes");
-//            }
-//            default -> System.out.println("Unknown command");
-//        }
-    
+
     	Command command = null;
-		
+
 		try {
 			command = this.applicationContext
 					.getBean(commandsEnum.toString(), Command.class);
@@ -77,41 +37,6 @@ public class RecipesCommandFactory implements RecipesCommandInterface {
         assert command != null;
         return command
 			.execute(commandBoundary);
-	
-    	
+
     }
-
-
-//    @Override
-//    public MiniAppCommandBoundary createRecipe(MiniAppCommandBoundary commandBoundary) {
-//        createRecipeCommand.execute(commandBoundary);
-//        return commandBoundary;
-//    }
-//
-//    @Override
-//    public MiniAppCommandBoundary modifyRecipe(MiniAppCommandBoundary commandBoundary) {
-//        return null;
-//    }
-//
-//    @Override
-//    public void deleteRecipe(MiniAppCommandBoundary commandBoundary) {
-//
-//    }
-//
-//    @Override
-//    public void deleteAllRecipes(MiniAppCommandBoundary commandBoundary) {
-//
-//    }
-//
-//    @Override
-//    public MiniAppCommandBoundary getRecipe(MiniAppCommandBoundary commandBoundary) {
-//        return null;
-//    }
-//
-//    @Override
-//    public MiniAppCommandBoundary getAllRecipe(MiniAppCommandBoundary commandBoundary) {
-//        return null;
-//    }
-
-
 }
