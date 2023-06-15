54603ce2687d0dae244ed0021d6366e215b5f4b5
amirmalamud <malamud.amir@s.afeka.ac.il>
2023-06-15 20:20:55 +0300

    name

--- src/main/java/superapp/Application.java
+++ src/main/java/superapp/Application.java
@@ -2,9 +2,6 @@ package superapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.data.mongodb.core.MongoTemplate;
-
 
 @SpringBootApplication
 public class Application {
